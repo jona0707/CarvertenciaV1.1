20d3a7bac8047b68301dd8dc0d18b399f5fe3f48
Jona <jona07072000@gmail.com>
2024-03-03 15:26:44 -0500

    Agregar controlador para validar los carros y el qr

--- Servidor/server/controller/Validar.controler.js
+++ Servidor/server/controller/Validar.controler.js
@@ -1,29 +1,25 @@
 const Carvertencia = require( "../models/Carvertencia.model" );
 const Comunidad = Carvertencia.Comunidad;
+
+
 module.exports.validarQR = async (request, response) => {
-    const { id, placa } = request.body;
+    const { id } = request.body;
 
     try {
-        //Buscar en todas las comunidades si el auto está asociado a alguna
-        const comunidades = await Comunidad.find({ autosComu: id });
+        // Buscar si el auto está asociado a alguna comunidad
+        const comunidad = await Comunidad.findOne({ autosComu: id });
 
-        if (comunidades.length > 0) {
-            //El auto está asociado a alguna comunidad por lo que tomo la primera
-            const comunidad = comunidades[0];
+        if (comunidad) {
+            // Si se encuentra una comunidad donde está el auto, devolver un mensaje positivo
             response.json({ resultado: true, comunidad: `Este auto pertenece a la comunidad: ${comunidad.nombreComu}` });
         } else {
-            //Si el auto no está asociado a ninguna comunidad, buscar en otras comunidades
-            const otrasComunidades = await Comunidad.find({ autosComu: { $ne: id } });
-            if (otrasComunidades.length > 0) {
-                //El auto está asociado a otra comunidad
-                const otraComunidad = otrasComunidades[0]; // Tomamos la primera comunidad encontrada
-                response.json({ resultado: true, comunidad: `Este auto pertenece a otra comunidad: ${otraComunidad.nombreComu}` });
-            } else {
-                //El auto no está asociado a ninguna comunidad
-                response.json({ resultado: false });
-            }
+            // Si no se encuentra ninguna comunidad donde está el auto, devolver un mensaje negativo
+            response.json({ resultado: false, message: 'El auto no está asociado a ninguna comunidad.' });
         }
     } catch (error) {
+        // Si ocurre un error durante la búsqueda, devolver un mensaje de error
+        console.error("Error al validar el QR:", error);
         response.status(500).json({ resultado: false, message: "Error al validar el QR." });
     }
 };
+
